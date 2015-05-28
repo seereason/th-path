5bbde28caeae8b2932d4475393c83280a7d43dd3
David Fox <dsf@seereason.com>
2015-05-28 13:27:20 -0700

    Use a type family for View instead of a regular type class

--- Language/Haskell/TH/Path.hs
+++ Language/Haskell/TH/Path.hs
@@ -1,7 +1,7 @@
 module Language.Haskell.TH.Path
     ( LensHint(..)
     , Field
-    , View(viewLens{-, ViewType-})
+    , View(viewLens, ViewType)
     , viewInstanceType
     , pathInstances
     , Path_Pair(..)
--- Language/Haskell/TH/Path/View.hs
+++ Language/Haskell/TH/Path/View.hs
@@ -5,8 +5,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 module Language.Haskell.TH.Path.View
-    ( -- View(viewLens, ViewType)
-      View(viewLens)
+    ( View(viewLens, ViewType)
     , viewInstanceType
     , viewTypes
     ) where
@@ -14,11 +13,12 @@ module Language.Haskell.TH.Path.View
 import Control.Applicative ((<$>))
 import Control.Lens (Lens')
 import Control.Monad.State (MonadState)
+import Data.Maybe (catMaybes)
+import Data.Set as Set (fromList, Set)
 import Language.Haskell.TH
 import Language.Haskell.TH.Context.Reify (InstMap, {-testContext,-} reifyInstancesWithContext, evalContextState)
 import Language.Haskell.TH.Desugar as DS (DsMonad)
 import Language.Haskell.TH.TypeGraph (E)
-import Language.Haskell.TH.TypeGraph.Core (typeArity)
 
 -- | If there is an instance of View for a pair of types, when @a@
 -- appears in the data, the lens returned by 'viewLens' is used to
@@ -26,27 +26,23 @@ import Language.Haskell.TH.TypeGraph.Core (typeArity)
 -- a User Interface, for example.  Then the resulting @b@ value is
 -- transformed back into an @a@ and stored in the database.  An
 -- instance of View is equivalent to a Substitute hint.
-#if 0
 class View a where
     type ViewType a
     viewLens :: Lens' a (ViewType a)
-#else
-class View a b | a -> b where
-    viewLens :: Lens' a b
-#endif
 
 viewInstanceType :: (DsMonad m, MonadState (InstMap (E Pred)) m) => Type -> m (Maybe Type)
 viewInstanceType typ =
-    do arity <- typeArity typ
-       v <- runQ $ VarT <$> newName "v"
-       vInsts <- if arity == 0 then reifyInstancesWithContext ''View [typ, v] else return []
+    do vInsts <- runQ $ reifyInstances ''ViewType [typ]
        case vInsts of
-         [(InstanceD _ (AppT (AppT _view _a) b) _)] -> return $ Just b
+         [TySynInstD _ (TySynEqn [_typ] type2)] -> return $ Just type2
          [] -> return Nothing
          _ -> error $ "Unexpected view instance(s): " ++ show vInsts
 
 -- | Any type that is part of a View instance
-viewTypes :: Q [Type]
-viewTypes = do
-  vInsts <- evalContextState $ reifyInstancesWithContext ''View [VarT (mkName "a"), VarT (mkName "b")]
-  return $ concatMap (\ (InstanceD _ (AppT (AppT (ConT _view) a) b) _) -> [a, b]) vInsts
+viewTypes :: Q (Set Type)
+viewTypes = evalContextState $ do
+  vInsts <- reifyInstancesWithContext ''View [VarT (mkName "a")]
+  -- trace ("vInsts a " ++ " -> " ++ show vInsts) (return ())
+  let aTypes = concatMap (\ (InstanceD _ (AppT (ConT _view) a) _) -> [a]) vInsts
+  bTypes <- catMaybes <$> mapM viewInstanceType aTypes
+  return $ Set.fromList $ aTypes ++ bTypes
