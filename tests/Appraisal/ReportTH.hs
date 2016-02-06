8a2b6541f358a51349b602090c88fbfd0a741151
David Fox <dsf@seereason.com>
2016-02-06 06:13:55 -0800

    Fix test code so it checks for changes in allDecs output

--- tests/Appraisal/ReportTH.hs
+++ tests/Appraisal/ReportTH.hs
@@ -14,20 +14,21 @@ module Appraisal.ReportTH where
 --import Appraisal.Markup (Markup(..))
 --import Appraisal.Permissions
 --import Appraisal.Report
-import Appraisal.ReportImage (MEUI)
+--import Appraisal.ReportImage (MEUI)
 import Appraisal.ReportInstances (startTypes)
 --import Appraisal.ReportItem
 --import Appraisal.ReportMap (ReportMap(ReportMap), ReportID)
 --import Appraisal.Utils.CIString (CIString(..))
 --import Control.Lens (Lens', Traversal', iso, _Just, _1, _2, _Left, _Right)
 --import Control.Monad.Writer (execWriterT)
---import Data.ByteString.UTF8 as UTF8 (toString)
---import Data.FileEmbed (embedFile)
+import Data.ByteString.UTF8 as UTF8 (toString)
+import Data.FileEmbed (embedFile)
 --import Data.Function (on)
 --import Data.Generics (Data, Typeable)
 --import Data.Int (Int64)
 import Data.List (isSuffixOf)
 --import Data.Map (Map)
+import Data.Monoid ((<>))
 --import Data.Text (Text)
 --import Data.UserId (UserId)
 --import Data.UUID (UUID)
@@ -62,9 +63,17 @@ import Prelude hiding (readFile)
 --import System.Posix.IO (handleToFd, closeFd)
 
 decs :: [Dec]
-decs = $(do let dir s = runIO (getDirectoryContents s) >>= mapM_ (addDependentFile) . map (s </>) . filter (isSuffixOf ".hs")
+decs = $(do addDependentFile "tests/ReportHead.hs"
+            addDependentFile "tests/ReportDecs.hs"
+            let dir s = runIO (getDirectoryContents s) >>= mapM_ (addDependentFile) . map (s </>) . filter (isSuffixOf ".hs")
             mapM_ dir ["Language/Haskell/TH/Path", "Language/Haskell/TH/Path/Decs"]
             decs' <- startTypes >>= runTypeGraphT allDecs -- >>= lift
             let code = (unlines . map (pprint . friendlyNames) . sort) decs'
-            runIO $ writeFile "tests/actual.hs" code
-            lift decs')
+            let hdr = UTF8.toString $(embedFile "tests/ReportHead.hs")
+                old = UTF8.toString $(embedFile "tests/ReportDecs.hs")
+                new = hdr <> code
+            case (new == old) of
+              True -> lift decs'
+              False ->
+                  do runIO (writeFile "tests/ReportDecs.hs.new" new)
+                     error "Generated tests/ReportDecs.hs does not match existing")
---
+++ tests/ReportHead.hs
@@ -0,0 +1,45 @@
+-- | Use template haskell functions to generate the path types for appraisalscribe.
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
+module ReportDecs where
+
+import Appraisal.File (File)
+import Appraisal.Image
+import Appraisal.ImageFile
+import Appraisal.IntJS
+import Appraisal.Markup (Markup(..))
+import Appraisal.Permissions
+import Appraisal.Report
+import Appraisal.ReportImage
+import Appraisal.ReportInstances
+import Appraisal.ReportItem
+import Appraisal.ReportMap (ReportID(..), ReportMap(..), MRR)
+import Appraisal.Utils.CIString (CIString(..))
+import Appraisal.Utils.Debug (trace'')
+import Control.Lens (iso, _Just, _1, _2, _Left, _Right, Lens', lens, prism', toListOf, Traversal', view)
+import Data.Generics (Data, Typeable)
+import Data.Int (Int64)
+import Data.Map (Map, toList)
+import Data.Proxy
+import Data.Text (Text)
+import Data.Tree (Tree(Node), Forest)
+import Data.UserId (UserId(UserId))
+import Data.UUID (UUID)
+import Data.UUID.Orphans ()
+import Language.Haskell.TH.Path.Core (IsPath(Path, pathsOf, toLens), IsPathNode(Peek, peek), IsPathType(idPath),
+                                      Path_Either(Path_Left, Path_Right), Path_Map(Path_Look),
+                                      Path_Maybe(Path_Just), Path_Pair(Path_First, Path_Second), mat)
+import Language.Haskell.TH.Path.Decs.Common (forestMap)
+import Language.Haskell.TH.Path.Order (lens_omat, Order, Path_OMap(Path_OMap, Path_At), toPairs)
+import Language.Haskell.TH.Path.View (View(viewLens))
+import Network.URI (nullURI, URI(URI), URIAuth(URIAuth))
+
